c837ef1c35f7d99eb156f3ad46adda101ea4f727
张宇豪 <84267606+CN-ZHANGYH@users.noreply.github.com>
2024-02-02 19:20:36 +0800

    添加公益活动记录和新增的功能页面：未完成

---
+++ GYCS-Admin-Vue/src/views/charity/ativitecreate/index.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="app-container">
+    <div>
+      <el-steps :space="200" :active="stepNumber" simple style="border-radius: 15px">
+        <el-step title="添加公益活动文章内容" :icon="Edit" />
+        <el-step title="设置公益活动基本功能" :icon="Setting" />
+        <el-step title="确认并发布公益活动" :icon="Position" />
+      </el-steps>
+    </div>
+
+    <div class="form-container">
+      <el-card class="step-card" >
+        <el-form v-if="stepNumber == 1" size="large" label-position="left">
+          <div style="display: flex;justify-content: space-between">
+            <!-- 表单内容 -->
+            <el-form-item label="活动标题">
+              <el-input placeholder="请输入活动标题"></el-input>
+            </el-form-item>
+            <el-form-item label="发布作者">
+              <el-input placeholder="请输入发布作者"></el-input>
+            </el-form-item>
+          </div>
+          <el-form-item label="活动描述">
+            <el-input type="textarea" placeholder="请输入当前的公益活动文章或者描述信息" :rows="20"></el-input>
+          </el-form-item>
+        </el-form>
+
+
+        <div class="form-button">
+          <el-button type="success" @click="stepNumber++">下一步</el-button>
+          <el-button type="danger" @click="stepNumber--">上一步</el-button>
+          <el-button type="primary" v-if="stepNumber == 3">确认</el-button>
+        </div>
+      </el-card>
+
+
+    </div>
+
+
+
+
+  </div>
+</template>
+
+
+<script setup>
+
+import {Edit, Position, Setting} from "@element-plus/icons-vue";
+
+const stepNumber = ref(1)
+
+
+</script>
+
+
+<style>
+.step-card {
+  margin-top: 30px;
+  width: 100%;
+  border-radius: 15px;
+  padding: 20px 20px;
+
+
+}
+
+.form-container {
+  display: flex;
+  justify-content: center;
+
+}
+
+.form-button{
+  text-align: center;
+}
+</style>
--- GYCS-Admin-Vue/src/views/charity/ativiterecord/index.vue
+++ GYCS-Admin-Vue/src/views/charity/ativiterecord/index.vue
@@ -1,53 +1,38 @@
 <template>
   <div class="app-container">
-    <div v-for="(activite,index) in activitieRecordList" :key="index">
-      <div style="width: 360px;height: 450px">
-        <div class="card-container">
-          <div class="circle"></div>
-          <div class="circle"></div>
-          <div class="card-inner">
-            <div class="card">
-              <div class="top">
-                <div style="overflow: hidden">
-                  <img src="@/assets/images/img.png" style="width: 100%;height: 100%;border-top-right-radius: 15px;border-top-left-radius: 15px">
-                </div>
-                <div class="title">
-                  <div>{{activite.title}}</div>
-                  <div style="display: flex;justify-content: space-between;margin-top: 15px">
-                    <button class="button">
-                      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 36 24">
-                        <path d="m18 0 8 12 10-8-4 20H4L0 4l10 8 8-12z"></path>
-                      </svg>
-                      评价：5星
-                    </button>
-                    <button class="Btn">
-
-                      <div class="sign"><svg viewBox="0 0 512 512"><path d="M377.9 105.9L500.7 228.7c7.2 7.2 11.3 17.1 11.3 27.3s-4.1 20.1-11.3 27.3L377.9 406.1c-6.4 6.4-15 9.9-24 9.9c-18.7 0-33.9-15.2-33.9-33.9l0-62.1-128 0c-17.7 0-32-14.3-32-32l0-64c0-17.7 14.3-32 32-32l128 0 0-62.1c0-18.7 15.2-33.9 33.9-33.9c9 0 17.6 3.6 24 9.9zM160 96L96 96c-17.7 0-32 14.3-32 32l0 256c0 17.7 14.3 32 32 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32l-64 0c-53 0-96-43-96-96L0 128C0 75 43 32 96 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32z"></path></svg></div>
-
-                      <div class="text">更多</div>
-                    </button>
-                  </div>
-                </div>
-                <div>
-
-                </div>
-              </div>
-              <p class="desc">
-                {{activite.desc}}
-              </p>
-            </div>
+    <div class="flex-container">
+      <div>
+        <div class="card-left">
+          <div class="card-content">
+            <p class="card-title">hover effect
+            </p><p class="card-para">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
           </div>
         </div>
       </div>
-    </div>
-    <pagination
-      v-show="total>0"
-      :total="total"
-      v-model:page="queryParams.pageNum"
-      v-model:limit="queryParams.pageSize"
-      @pagination=""
-    />
+      <div class="right-container" style="width: 80%;height: 600px" >
+        <template v-for="item in activitieRecordList">
+          <div class="card " >
+            <div class="header">
+              <img src="@/assets/images/img.png" class="card-image">
+            </div>
+            <div class="info">
+              <p class="title">灾区公益活动</p>
+            </div>
+            <div class="footer">
+              <p class="tag">⭐反馈：5星</p>
+              <button class="button">
+                <svg class="svgIcon" viewBox="0 0 384 512">
+                  <path
+                      d="M214.6 41.4c-12.5-12.5-32.8-12.5-45.3 0l-160 160c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 141.2V448c0 17.7 14.3 32 32 32s32-14.3 32-32V141.2L329.4 246.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-160-160z"
+                  ></path>
+                </svg>
+              </button>
 
+            </div>
+          </div>
+        </template>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -81,6 +66,16 @@ const activitieRecordList = ref([
     title: "地震灾区",
     desc: "这是地震的灾区"
   },
+  {
+    id: 6,
+    title: "地震灾区",
+    desc: "这是地震的灾区"
+  },
+  {
+    id: 7,
+    title: "地震灾区",
+    desc: "这是地震的灾区"
+  },
 ]);
 
 const total = ref(0);
@@ -103,216 +98,217 @@ const { queryParams, form, rules } = toRefs(data);
 
 
 </script>
-<style scoped>
-.card {
+
+
+<style>
+.flex-container {
+  display: flex;
+  justify-content: space-between;
+}
+
+
+.right-container {
+  width: 85%;
+  height: 600px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: flex-start;
+}
+
+.card-left {
+  width: 300px;
+  height: 200px;
+  margin-right: 40px;
+  background-color: #FF3CAC;
+  background-image: linear-gradient(225deg, #FF3CAC 0%, #784BA0 50%, #2B86C5 100%);
+  color: white;
+  border-radius: 8px;
   overflow: hidden;
-  width: 290px;
-  height: 404px;
-  background: linear-gradient(10deg, rgba(38,146,255,1) 0%, rgba(0,63,255,1) 100%);
-  border-radius: 16px;
-  box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
-  backdrop-filter: blur(10.5px);
-  -webkit-backdrop-filter: blur(10.5px);
-  border: 1px solid rgba(255, 255, 255, 0.3);
-}
-
-.top {
-  background: rgba(255, 255, 255, 0.2);
-  border-radius: 16px;
-  box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
-  backdrop-filter: blur(10.5px);
-  -webkit-backdrop-filter: blur(10.5px);
-  border: 1px solid rgba(255, 255, 255, 0.3);
-  width: 290px;
-  height: 320px;
-}
-
-.top .title {
-  color: #fff;
-  font-weight: bolder;
-  font-size: x-large;
-  margin-left: 10px;
-  margin-top: 1%
-}
-
-.card .desc {
-  color: #fff;
-  opacity: 75%;
-  font-size: small;
-  font-weight: lighter;
-  margin-left: 10px;
-  margin-top: 2%
-}
-
-.top img {
-  width: 100%;
-  height: 100%;
-  object-fit: cover; /* 保持图片比例并填充整个容器 */
-  transition: transform 0.3s ease; /* 添加过渡动画效果 */
-}
-
-.top:hover img {
-  transform: scale(1.1); /* 鼠标悬停时应用内部放大效果 */
-}
-
-
-.card-container {
-  width: 305px;
-  height: 415px;
-  transition: all 0.2s;
   position: relative;
+  perspective: 1000px;
+  transition: all 0.5s cubic-bezier(0.23, 1, 0.320, 1);
   cursor: pointer;
 }
 
-.card-inner {
-  width: inherit;
-  height: inherit;
-  background: rgba(255, 255, 255, 0.05);
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.25);
-  backdrop-filter: blur(10px);
-  border-radius: 8px;
+.card-content {
+  padding: 10px;
+  position: relative;
+  z-index: 1;
+}
+
+.card-left:hover {
+  transform: scale(1.05);
+  box-shadow: 0 10px 20px rgba(0, 0, 0, 0.2);
+}
+
+.card-left:before {
+  content: "";
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 60%;
+  background-color: rgba(0, 0, 0, 0.1);
+  opacity: 0;
+  transition: all 0.5s cubic-bezier(0.23, 1, 0.320, 1);
+  z-index: 1;
+}
+
+.card-left:hover:before {
+  opacity: 1;
+}
+
+.card-left .card-title {
+  font-size: 24px;
+  font-weight: 700;
+  margin-bottom: 8px;
+  text-transform: capitalize;
+}
+
+.card-left .card-para {
+  font-size: 16px;
+  opacity: 0.8;
+}
+
+.card-content {
+  transform: translateY(50%);
+  transition: all 0.5s cubic-bezier(0.23, 1, 0.320, 1);
+}
+
+.card-left:hover .card-content {
+  transform: translateY(0%);
+}
+
+.card {
+  border: 2px solid rgb(99 102 241);
+  margin-right: 10px;
+  margin-bottom: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  border-radius: 0.75rem;
+  background-color: white;
+  width: 290px;
+  height: 370px;
+  box-shadow: 0 4px 6px -1px rgba(0,0,0,.1),
+  0 2px 4px -2px rgba(0,0,0,.1);
+}
+
+.header {
+  position: relative;
+  background-clip: border-box;
+  margin-top: 1.5rem;
+  margin-left: 1rem;
+  margin-right: 1rem;
+  border-radius: 0.75rem;
+  background-color: rgb(33 150 243);
+  box-shadow: 0 10px 15px -3px rgba(33,150,243,.4),0 4px 6px -4px rgba(33,150,243,.4);
+  height: 197px;
   display: flex;
   justify-content: center;
-  align-items: center; /* 上下居中 */
+  align-items: center;
+}
+
+.info {
+  border: none;
+  padding: 1rem;
+  text-align: left;
+  margin-left: 10px;
+}
+
+.title {
+  color: rgb(38 50 56);
+  letter-spacing: 0;
+  line-height: 1.375;
+  font-weight: 600;
+  font-size: 1.1rem;
+  margin-bottom: 0.5rem;
+}
+
+.footer {
+  padding: 0.75rem;
+  border: 1px solid rgb(236 239 241);
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  background-color: rgba(0, 140, 255, 0.082);
+  border-bottom-right-radius: 10px;
+  border-bottom-left-radius: 10px;
+}
+
+.tag {
+  font-weight: 300;
+  font-size: .75rem;
+  display: block;
+}
+
+
+.card-image {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  border-radius: 10px;
 }
 
 
 
-.circle {
-  width: 100px;
-  height: 100px;
-  background: repeating-linear-gradient(48deg, #3023ae 0%, #ff0099 100%);
-  border-radius: 35% 30% 75% 30% / 49% 30% 70% 51%;
-  position: absolute;
-  animation: move-up6 2s ease-in infinite alternate-reverse;
-}
 
-.circle:nth-child(1) {
-  top: -25px;
-  left: -25px;
-}
-
-.circle:nth-child(2) {
-  bottom: -25px;
-  right: -25px;
-  animation-name: move-down1;
-}
-
-@keyframes move-up6 {
-  to {
-    transform: translateY(-10px);
-  }
-}
-
-@keyframes move-down1 {
-  to {
-    transform: translateY(10px);
-  }
-}
 
 .button {
-  width: fit-content;
-  display: flex;
-  padding: 1.2em 1rem;
-  cursor: pointer;
-  gap: 0.4rem;
-  font-weight: bold;
-  border-radius: 30px;
-  text-shadow: 2px 2px 3px rgb(136 0 136 / 50%);
-  background: linear-gradient(15deg, #880088, #aa2068, #cc3f47, #de6f3d, #f09f33, #de6f3d, #cc3f47, #aa2068, #880088) no-repeat;
-  background-size: 300%;
-  color: #fff;
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  background-color: rgb(20, 20, 20);
   border: none;
-  background-position: left center;
-  box-shadow: 0 30px 10px -20px rgba(0,0,0,.2);
-  transition: background .3s ease;
+  font-weight: 600;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  box-shadow: 0px 0px 0px 4px rgba(180, 160, 255, 0.253);
+  cursor: pointer;
+  transition-duration: 0.3s;
+  overflow: hidden;
+  position: relative;
+}
+
+.svgIcon {
+  width: 12px;
+  transition-duration: 0.3s;
+}
+
+.svgIcon path {
+  fill: white;
 }
 
 .button:hover {
-  background-size: 320%;
-  background-position: right center;
-}
-
-.button:hover svg {
-  fill: #fff;
-}
-
-.button svg {
-  width: 23px;
-  fill: #f09f33;
-  transition: .3s ease;
-}
-
-
-
-.Btn {
-  margin-top: 3px;
-  margin-right: 20px;
-  display: flex;
+  width: 140px;
+  border-radius: 50px;
+  transition-duration: 0.3s;
+  background-color: rgb(33 150 243);
   align-items: center;
-  justify-content: flex-start;
-  width: 45px;
-  height: 45px;
-  border: none;
-  border-radius: 50%;
-  cursor: pointer;
-  position: relative;
-  overflow: hidden;
-  transition-duration: .3s;
-  box-shadow: 2px 2px 10px rgba(0, 0, 0, 0.199);
-  background-color: rgb(255, 65, 65);
 }
 
-/* plus sign */
-.sign {
-  width: 100%;
-  transition-duration: .3s;
-  display: flex;
-  align-items: center;
-  justify-content: center;
+.button:hover .svgIcon {
+  /* width: 20px; */
+  transition-duration: 0.3s;
+  transform: translateY(-200%);
 }
 
-.sign svg {
-  width: 17px;
-}
-
-.sign svg path {
-  fill: white;
-}
-/* text */
-.text {
+.button::before {
   position: absolute;
-  right: 0%;
-  width: 0%;
-  opacity: 0;
+  bottom: -20px;
+  content: "查看更多";
   color: white;
-  font-size: 1.2em;
-  font-weight: 600;
-  transition-duration: .3s;
-}
-/* hover effect on button width */
-.Btn:hover {
-  width: 125px;
-  border-radius: 40px;
-  transition-duration: .3s;
+  /* transition-duration: .3s; */
+  font-size: 0px;
 }
 
-.Btn:hover .sign {
-  width: 30%;
-  transition-duration: .3s;
-  padding-left: 20px;
-}
-/* hover effect button's text */
-.Btn:hover .text {
+.button:hover::before {
+  font-size: 13px;
   opacity: 1;
-  width: 70%;
-  transition-duration: .3s;
-  padding-right: 10px;
+  bottom: unset;
+  /* transform: translateY(-30px); */
+  transition-duration: 0.3s;
 }
-/* button click effect*/
-.Btn:active {
-  transform: translate(2px ,2px);
-}
+
 </style>
-
-
