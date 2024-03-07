b34049d07e895fbe039952e70095f523d895f0b2
Yyz <2756864799@qq.com>
2024-03-07 13:05:39 +0800

    初始化项目前端

---
+++ GYCS-Charity-Vue/src/main.js
@@ -0,0 +1,20 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import Vuesax from 'vuesax-alpha'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+
+import 'vuesax-alpha/theme-chalk/index.css'
+// dark mode
+// import 'vuesax-alpha/theme-chalk/dark/css-vars.css'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+app.use(Vuesax)
+
+app.mount('#app')
